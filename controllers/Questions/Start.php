09c8f991f785fa231068fcca04c01b276ddfa6a7
iTechCydia <vincent.bathelier98@gmail.com>
2018-10-04 19:15:49 +0200

    Le quizz démarre correctement

--- controllers/Questions/Start.php
+++ controllers/Questions/Start.php
@@ -7,8 +7,19 @@ class Start
     public function index()
     {
         if (isset($_POST['pseudo'])) {
-            session_start();
+            if (isset($_COOKIE[session_name()])) {
+                $_SESSION = [];
+                session_destroy();
+                session_start();
+            } else {
+                session_start();
+            }
+            
             $_SESSION['user'] = htmlspecialchars($_POST['pseudo']);
+            $_SESSION['user'] = [
+                'question' => []
+            ];
+            
             header('Location: /game/1');
         } else {
             header('Location: /game');
